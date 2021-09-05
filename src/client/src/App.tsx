e25b59f688ca058b0a417819f3094bda4ff46d2d
Sebastian <smilhas@itba.edu.ar>
2021-09-05 19:20:51 -0300

    update not found route configuration

--- src/client/src/App.tsx
+++ src/client/src/App.tsx
@@ -1,21 +1,22 @@
-import React, { Switch, Route} from 'react-router-dom'
+import React, { Switch, Route, Redirect} from 'react-router-dom'
 import './App.css'
 import SiteWrapper from './components/layout/SiteWrapper'
 import {Regalos, LandingPage, LandingPageDev} from './components/sections/index'
 import MaintenancePage from './components/MaintenancePage'
 
-
 function App(): JSX.Element {
-
 	return (
-		<SiteWrapper>
-			<Switch>
-				<Route exact path='/' component={LandingPage} />
-				<Route exact path='/dev' component={LandingPageDev} />
-				<Route exact path='/test' component={Regalos} />
-				<Route path='/' component={MaintenancePage} />
-			</Switch>
-		</SiteWrapper>
+		<Switch>
+			<Route exact path='/not-found' component={MaintenancePage} />
+			<SiteWrapper>
+				<Switch>
+					<Route exact path='/' component={LandingPage}/>
+					<Route exact path='/dev' component={LandingPageDev} />
+					<Route exact path='/test' component={Regalos} />
+					<Redirect to='/not-found' />
+				</Switch>
+			</SiteWrapper>
+		</Switch>
 	)
 }
 
