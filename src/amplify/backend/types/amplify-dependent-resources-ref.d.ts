ef4502a2cdca493322d4c3406c88eefa4790998e
Sebastian <smilhas@itba.edu.ar>
2021-12-11 02:15:56 -0300

    add amplify

---
+++ src/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,34 @@
+export type AmplifyDependentResourcesAttributes = {
+    "api": {
+        "24kserver": {
+            "GraphQLAPIKeyOutput": "string",
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    },
+    "auth": {
+        "24kserverdd8115a5": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    },
+    "storage": {
+        "content": {
+            "BucketName": "string",
+            "Region": "string"
+        }
+    },
+    "hosting": {
+        "S3AndCloudFront": {
+            "Region": "string",
+            "HostingBucketName": "string",
+            "WebsiteURL": "string",
+            "S3BucketSecureURL": "string"
+        }
+    }
+}
