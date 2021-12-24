0db59391365cfedb608e5a1ec9014bec44b8deee
Sebastian <smilhas@itba.edu.ar>
2021-12-24 13:15:46 -0300

    new visuals for the site

--- src/client/src/components/layout/SiteWrapper.tsx
+++ src/client/src/components/layout/SiteWrapper.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
-import { Layout, Menu, PageHeader, Descriptions, Button, Divider, Space, BackTop } from 'antd'
+import { Layout, Menu, PageHeader, Descriptions, Button, Divider, Space, BackTop, Typography } from 'antd'
 import { HomeOutlined, GiftOutlined, AppstoreOutlined, SettingOutlined } from '@ant-design/icons'
 
 import Amplify, { API, graphqlOperation, Auth } from 'aws-amplify'
@@ -10,15 +10,38 @@ import './SiteWrapper.css'
 
 const { SubMenu } = Menu
 const { Header, Content, Footer } = Layout
+const { Title, Text } = Typography
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 function SiteWrapper(props: React.ComponentProps<any>):JSX.Element {
 
+	const calculateTimeLeft = () => {
+		const year = new Date().getFullYear()
+		const difference = +new Date('03/19/2022') - +new Date()
+
+		let timeLeft = '00 , 00:00:00'
+
+		if (difference > 0) {
+			const days = Math.floor(difference / (1000 * 60 * 60 * 24))
+			const hours = Math.floor((difference / (1000 * 60 * 60)) % 24)
+			const minutes = Math.floor((difference / 1000 / 60) % 60)
+			const seconds = Math.floor((difference / 1000) % 60)
+			timeLeft = `${days} , ${hours}:${minutes}:${seconds}`
+		}
+		return timeLeft
+	}
+
+	const [timeLeft, setTimeLeft] = useState(calculateTimeLeft())
 	const [authenticated, setAuthState] = useState(false)
 
 	useEffect(() => {
 		fetchUser()
-	}, [])
+		const timer = setTimeout(() => {
+			setTimeLeft(calculateTimeLeft())
+		}, 1000)
+
+		return () => clearTimeout(timer)
+	})
 
 	const randomString = (): string => {
 		let result = ''
@@ -42,52 +65,19 @@ function SiteWrapper(props: React.ComponentProps<any>):JSX.Element {
 			<Layout
 				className='layout'
 			>
+				<BackTop />
 				<Header 
-					className='header'
-					style={{ padding: '0 50px' }}
+					// className='header'
+					// style={{ padding: '0 50px' }}
+					className='header-menu'
 				>
-					<div
-						style={{width: '100%', background: '#001529', padding: '0 50px'}}
-					>
-						<a
-							href='https://github.com/smilhas/24k'
-							target='_blank'
-							rel='noopener noreferrer'
-							className='my-link'
-						>
-							Go checkout my Github →
-						</a>
-						<Space style={{float: 'right'}}>
-							{/* <Button
-								key='2'
-							>
-								⚙
-							</Button> */}
-							{ authenticated &&
-								<Button
-									key='1'
-									type='primary'
-									onClick={() => Auth.signOut()}
-								>
-									Logout
-								</Button>
-							}
-
-						</Space>
-					</div>
-				</Header>
-				<Content style={{ padding: '0 50px' }}>
-					<PageHeader
-						ghost={false}
-						title='Tini y Sebas 👰🤵'
-						subTitle='19 de Marzo 2022'
-					/>
 					<Menu
-						theme='light'
+						className='header-menu'
+						theme='dark'
 						mode='horizontal'
 					>
 						<Menu.Item
-							key='home'
+							key='home_item'
 							icon={<HomeOutlined />}
 						>
 							<Link to='/' target=''>
@@ -95,7 +85,7 @@ function SiteWrapper(props: React.ComponentProps<any>):JSX.Element {
 							</Link>
 						</Menu.Item>
 						<Menu.Item
-							key='regalos'
+							key='regalos_item'
 							icon={<GiftOutlined />}
 						>
 							<Link to='/regalos' target=''>
@@ -104,7 +94,11 @@ function SiteWrapper(props: React.ComponentProps<any>):JSX.Element {
 						</Menu.Item>
 						{ authenticated &&
 							<>
-								<SubMenu key='SubMenu' icon={<SettingOutlined />} title='Test'>
+								<SubMenu
+									key='test_submenu'
+									icon={<SettingOutlined />}
+									title='Test'
+								>
 									<Menu.ItemGroup title='Item 1'>
 										<Menu.Item key='setting:1'>
 											<Link to={`/${randomString()}`} target=''>
@@ -117,17 +111,55 @@ function SiteWrapper(props: React.ComponentProps<any>):JSX.Element {
 										<Menu.Item key='setting:3'>Option 3</Menu.Item>
 										<Menu.Item key='setting:4'>Option 4</Menu.Item>
 									</Menu.ItemGroup>
-								</SubMenu><Menu.Item key='alipay' icon={<AppstoreOutlined />}>
+								</SubMenu>
+								<Menu.Item
+									key='alipay'
+									icon={<AppstoreOutlined />}
+								>
 									<Link to='/dev' target=''>
 										Dev site
 									</Link>
 								</Menu.Item>
 							</>
 						}
+						<Menu.Item
+							key='logout_item'
+							className='menu-item-right'
+						>
+							<Button
+								key='1'
+								type='primary'
+								onClick={() => Auth.signOut()}
+							>
+										Logout
+							</Button>
+						</Menu.Item>
 					</Menu>
-					<div className='site-layout-content'>
-						{props.children}
-					</div>
+				</Header>
+				<Content className='site-layout-content'>
+					<br/>
+					<br/>
+					<br/>
+					<br/>
+					<br/>
+					<br/>
+					<br/>
+					<br/>
+					<br/>
+					<br/>
+					<br/>
+					<br/>
+					<br/>
+					<br/>
+					<br/>
+					<br/>
+					<br/>
+					<br/>
+					<Header className='site-layout-header'>
+						<Title level={2} className='title-header'>TINI Y SEBAS</Title>
+						<Title level={3} className='title-header'>{timeLeft}</Title>
+					</Header>
+					{props.children}
 				</Content>
 				<Footer style={{ textAlign: 'center' }}>
 					🏄‍♂️
@@ -140,6 +172,14 @@ function SiteWrapper(props: React.ComponentProps<any>):JSX.Element {
 							🌎 Buenos Aires, Argentina
 						</Descriptions.Item>
 					</Descriptions>
+					<a
+						href='https://github.com/smilhas/24k'
+						target='_blank'
+						rel='noopener noreferrer'
+						className='my-link'
+					>
+							Go checkout my Github →
+					</a>
 				</Footer>
 			</Layout>
 		</div>
